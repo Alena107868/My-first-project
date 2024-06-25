c7132db7dd3390d809e4b7c086df6044820a6197
Alena107868 <pogodaewa.ale@yandex.ru>
2024-06-25 11:45:12 +0300

    Написала функцию для определения количества определенных слов постранично

--- project.py
+++ project.py
@@ -1,11 +1,16 @@
-def exemple():
-    print('hello world')
+
+import PyPDF2
+
+reader = PyPDF2.PdfReader('King_S._Pod_Kupolom.a4.pdf')
+print(len(reader.pages))
+print(reader.pages[25].extract_text())
+print(reader.pages[27].extract_text())
+
+l = reader.pages[27].extract_text().lower().split()
+print({b: l.count(b.lower()) for b in ("Младший", "закричал", " - Лживая сука! ")})
 
 
-def introduser():
-    print('my name is mrs fuck you')
 
-    print('123')
 
-if __name__ == '__main__':
-    introduser()
+
+
