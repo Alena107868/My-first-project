efab97124c4216e73b6539f5bd975273d5822d8c
Alena107868 <pogodaewa.ale@yandex.ru>
2024-06-25 21:25:15 +0300

    замена слов

--- project.py
+++ project.py
@@ -1,25 +1,18 @@
-<<<<<<< HEAD
+import string
 
 import PyPDF2
+import string
 
 reader = PyPDF2.PdfReader('King_S._Pod_Kupolom.a4.pdf')
 print(len(reader.pages))
-print(reader.pages[260].extract_text())
+print(reader.pages[27].extract_text())
 
-l = reader.pages[260].extract_text().lower().split()
-print({b: l.count(b.lower()) for b in ("Пайпер", "Бренда", "Джулия", 'Овчарка', 'Литл-Битч.')})
-
-
-=======
->>>>>>> 32057f9c8aaea7026bc870059d2baad2cf978abd
-
-import PyPDF2
-
-<<<<<<< HEAD
+l = reader.pages[27].extract_text().lower()
+print({b: l.count(b.lower()) for b in ("Младший", " Лживая сука", "Энджи", 'Овчарка', 'Литл-Битч')})
+phrase = 'Лживая сука'
+substituted_phrase = phrase.replace('Лживая сука', 'Обманщица')
+print(phrase)
+print(substituted_phrase)
 
 
-=======
-reader = PyPDF2.PdfReader('King_S._Pod_Kupolom.a4.pdf')
-print(len(reader.pages))
-print(reader.pages[25].extract_text())
->>>>>>> 32057f9c8aaea7026bc870059d2baad2cf978abd
+
