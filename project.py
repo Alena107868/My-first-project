245cc23974117ffb8a9694abe4fbc17561fc16c1
Alena107868 <pogodaewa.ale@yandex.ru>
2024-06-27 14:34:11 +0300

    .

--- project.py
+++ project.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import PyPDF2
 
@@ -10,16 +9,6 @@ l = reader.pages[260].extract_text().lower().split()
 print({b: l.count(b.lower()) for b in ("Пайпер", "Бренда", "Джулия", 'Овчарка', 'Литл-Битч.')})
 
 
-=======
->>>>>>> 32057f9c8aaea7026bc870059d2baad2cf978abd
-
-import PyPDF2
-
-<<<<<<< HEAD
 
 
-=======
-reader = PyPDF2.PdfReader('King_S._Pod_Kupolom.a4.pdf')
-print(len(reader.pages))
-print(reader.pages[25].extract_text())
->>>>>>> 32057f9c8aaea7026bc870059d2baad2cf978abd
+
